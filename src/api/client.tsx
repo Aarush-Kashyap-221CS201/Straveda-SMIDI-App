89f09b77108445e4c141d6ef6e56da745fd5bc23
Aditya Gaikwad <adityabgaikwad0012@gmail.com>
2025-11-20 19:50:39 +0530

    Changed Client Link File

--- src/api/client.tsx
+++ src/api/client.tsx
@@ -1,27 +1,10 @@
-// // src/api/client.js
-// import axios from "axios";
+// src/api/client.js
+import axios from "axios";
 
-// // const BASE_URL = "https://simdi-fertilizers.onrender.com";
+const BASE_URL = "https://simdi-fertilizers.onrender.com";
 
 
 // const BASE_URL = "http://localhost:5001";
-
-// const api = axios.create({
-//   baseURL: BASE_URL,
-//   timeout: 15000,
-//   headers: {
-//     "Content-Type": "application/json",
-//     Accept: "application/json",
-//   },
-// });
-
-// export default api;
-
-
-
-import axios from "axios";
-
-const BASE_URL = "http://10.0.2.2:5001";
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -33,3 +16,21 @@ const api = axios.create({
 });
 
 export default api;
+
+
+//FOR LOCAL DEVLOPMENT
+
+// import axios from "axios";
+
+// const BASE_URL = "http://10.0.2.2:5001";
+
+// const api = axios.create({
+//   baseURL: BASE_URL,
+//   timeout: 15000,
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+// });
+
+// export default api;
